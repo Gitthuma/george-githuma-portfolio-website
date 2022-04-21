4b7df870cd1dd188ad63494cb208fdb90c4b6e51
George Githuma Njogu <george.githuma@gmail.com>
2022-04-22 00:16:30 +0300

    Import Created components in order of how they will appear on the site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 //Create App component
 import React from 'react'
+import Header from './Components/Header/Header'
+import Nav from './Components/Nav/Nav'
+import About from './Components/About/About'
+import Experience from './Components/Experience/Experience'
+import Services from './Components/Services/Services'
+import Portfolio from './Components/Portfolio/Portfolio'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contacts from './Components/Contacts/Contacts'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
-    <div>App</div>
+
+    //Import Created components in order of how 
+    //they will appear on the site
+    //Wrap them in a react fragment
+
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <Contacts />
+            <Footer />
+        </>
+
   )
 }
 
