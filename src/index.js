b63393664a210c914c50dff4fad68c62d5a3a9e9
George Githuma Njogu <george.githuma@gmail.com>
2022-04-21 17:47:58 +0300

    Added some formating

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 //import react
-import React from "react";
-
 //import ReactDOM
-import ReactDOM  from "react-dom";
-
 //import App component
-import App from './App';
-
 //import index.css
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from './App';
 import './index.css'
+
 
 //Render App component
 //document.querySelector gets root and renders it inside App component
