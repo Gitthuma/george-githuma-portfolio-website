a5a4920ebb8c02d6383641befb45446868018ca0
George Githuma Njogu <george.githuma@gmail.com>
2022-04-21 14:00:12 +0300

    import App component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import React from "react";
 //import ReactDOM
 import ReactDOM  from "react-dom";
 
+//import App component
+import App from './App';
+
 //Render App component
 //document.querySelector gets root and renders it inside App component
 ReactDOM.render(<App />, document.querySelector("#root"))
