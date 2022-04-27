d9f6db1c0b0b84d9e9cee582716a21cd7254930f
George Githuma Njogu <george.githuma@gmail.com>
2022-04-27 15:58:42 +0300

    Import newly created CTA and render it

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Header.css'
+//Import newly created CTA
+import CTA from './CTA'
 
 function Header() {
   return (
@@ -9,6 +11,9 @@ function Header() {
         <h5>Hello I'm</h5>
         <h1>George Githuma Njogu</h1>
         <h5 className='text-light'>Software Developer</h5>
+
+        {/*Render CTA*/}
+        <CTA />
       </div>
 
     </header>
