8e328239e9ce728829ec6cc1258b74768698db9a
George Githuma Njogu <george.githuma@gmail.com>
2022-04-27 15:36:42 +0300

    Create intro title

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,15 @@ import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      {/*Create intro title*/}
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>George Githuma Njogu</h1>
+        <h5 className='text-light'>Software Developer</h5>
+      </div>
+
+    </header>
   )
 }
 
