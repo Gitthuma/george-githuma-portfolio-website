ce8d558588e7c10efaaa34b7b239cbd6a36f3cc9
George Githuma Njogu <george.githuma@gmail.com>
2022-05-02 01:45:26 +0300

    If we are to click about, we want to move active class to about using onClick function and className function

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -13,13 +13,14 @@ import {useState} from 'react'
 //The use state value will be active nav and the update function will be setActiveNav
 //This will point to a default that will bring us to the top of the page
 // If activeNav is = to # then give it the ctive class if not give it nothing
+//If we are to click about, we want to move active class to about using onClick function and className function
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
       {/*Added icons to the nav component*/}
       <a href='#' className ={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
-      <a href='#about'><AiOutlineUser /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className ={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#experience'><BiBook /></a>
       <a href='#services'><RiServiceLine /></a>
       <a href='#contact'><BiMessageSquareDetail /></a>
