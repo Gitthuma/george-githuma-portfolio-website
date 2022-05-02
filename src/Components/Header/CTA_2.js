e56b6d29771a0ee05aa4132d378c2c59c21580b2
george.githuma <george.githuma@gmail.com>
2022-05-03 00:29:14 +0300

    Change link to cta-2

--- src/Components/Header/CTA_2.js
+++ src/Components/Header/CTA_2.js
@@ -1,4 +1,6 @@
 import React from 'react'
+//import cv from assets
+import Resume from '../../Assets/George-Githuma-Resume.pdf'
 
 function CTA_2() {
   return (
@@ -6,8 +8,9 @@ function CTA_2() {
     <div className='cta_2'>
         {/*Remove download botton*/}
 
-        {/*The href is local to the page, we will give the contact section an id of same so that when this is clicked it leads there*/}
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        {/*Assign import with a download attribute to make it downloadable*/}
+        {/*Add btn and btn primary class to make them buttons*/}
+        <a href={Resume} download className='btn'>Download Resume</a>
     </div>
   )
 }
