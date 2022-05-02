2505c5bb6bf49e4bc0c3f7f6e4342134de10d8f8
george.githuma <george.githuma@gmail.com>
2022-05-03 00:06:27 +0300

    Created component CTA_2

--- src/Components/Header/CTA_2.js
+++ src/Components/Header/CTA_2.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function CTA_2() {
   return (
-    <div>CTA_2</div>
+    <div className='cta'>
+        {/*Assign import with a download attribute to make it downloadable*/}
+        {/*Add btn and btn primary class to make them buttons*/}
+        <a href={Resume} download className='btn'>Download Resume</a>
+        {/*The href is local to the page, we will give the contact section an id of same so that when this is clicked it leads there*/}
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+    </div>
   )
 }
 
