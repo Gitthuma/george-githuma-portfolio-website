f82476873a1dc239b8ba16453062e9aefd32e550
george.githuma <george.githuma@gmail.com>
2022-05-02 20:04:59 +0300

    Create div for about__container

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,9 +5,14 @@ function About() {
   return (
     //Change div to section and add id
     //Create heading for the about section
+    //Create div for about__container
     <section id='about'>
       <h5>Get to Know</h5>
       <h2>About Me</h2>
+
+      <div className='container about__container'>
+
+      </div>
     
     </section>
   )
