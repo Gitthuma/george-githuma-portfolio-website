00e481bfabd2172875ad9e8b16bdd7c31e9b76c2
george.githuma <george.githuma@gmail.com>
2022-05-02 20:01:14 +0300

    Create heading for the about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,12 @@ import './About.css'
 function About() {
   return (
     //Change div to section and add id
-    <section id='about'>About</section>
+    //Create heading for the about section
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+    
+    </section>
   )
 }
 
