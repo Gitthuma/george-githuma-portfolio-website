a3bd6a2415525fe1cdb33bfd199198005cbdbd6b
george.githuma <george.githuma@gmail.com>
2022-05-02 20:26:11 +0300

    Add content headings and icons

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import myImg from '../../Assets/George-Githuma-Njogu-img.png'
+import {FaAward} from 'react-icons/fa'
 
 function About() {
   return (
@@ -10,6 +11,7 @@ function About() {
     //Create divs for the image and the content
     //Add image into the image div
     //Add about cards with articles
+    //Add content headings and icons
     <section id='about'>
       <h5>Get to Know</h5>
       <h2>About Me</h2>
@@ -25,7 +27,9 @@ function About() {
         <div className='about__content'>
           <div className='about__cards'>
             <article className='about__card'>
-
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
             </article>
           </div>
         </div>
