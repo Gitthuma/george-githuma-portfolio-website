2dc605d0068191735fecf7561a7ef728a18e5460
george.githuma <george.githuma@gmail.com>
2022-05-02 20:07:57 +0300

    Create divs for the image and the content

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,11 +6,19 @@ function About() {
     //Change div to section and add id
     //Create heading for the about section
     //Create div for about__container
+    //Create divs for the image and the content
     <section id='about'>
       <h5>Get to Know</h5>
       <h2>About Me</h2>
 
       <div className='container about__container'>
+        <div className='about__me'>
+
+        </div>
+
+        <div className='about__content'>
+
+        </div>
 
       </div>
     
