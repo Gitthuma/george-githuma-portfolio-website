0d317b0d8b41f061944eba75ddb3ca04d0d4b88e
george.githuma <george.githuma@gmail.com>
2022-05-02 20:35:26 +0300

    Duplicate icons two times and change content

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './About.css'
 import myImg from '../../Assets/George-Githuma-Njogu-img.png'
 import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 function About() {
   return (
@@ -12,6 +14,7 @@ function About() {
     //Add image into the image div
     //Add about cards with articles
     //Add content headings and icons
+    //Duplicate icons two times and change content
     <section id='about'>
       <h5>Get to Know</h5>
       <h2>About Me</h2>
@@ -26,11 +29,25 @@ function About() {
 
         <div className='about__content'>
           <div className='about__cards'>
+
             <article className='about__card'>
               <FaAward className='about__icon'/>
               <h5>Experience</h5>
               <small>2+ Years Working</small>
             </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>10+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5+ Completed</small>
+            </article>
+
           </div>
         </div>
 
