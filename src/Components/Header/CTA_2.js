69dba8f41c5cdf20022fe9a9a20c06242a1c3fc2
george.githuma <george.githuma@gmail.com>
2022-05-03 00:13:17 +0300

    Remove download botton

--- src/Components/Header/CTA_2.js
+++ src/Components/Header/CTA_2.js
@@ -4,9 +4,8 @@ function CTA_2() {
   return (
       //Change class to cta_2
     <div className='cta_2'>
-        {/*Assign import with a download attribute to make it downloadable*/}
-        {/*Add btn and btn primary class to make them buttons*/}
-        <a href={Resume} download className='btn'>Download Resume</a>
+        {/*Remove download botton*/}
+
         {/*The href is local to the page, we will give the contact section an id of same so that when this is clicked it leads there*/}
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
