156bea92c53b100f676c91e97dbff127c4f74c85
george.githuma <george.githuma@gmail.com>
2022-05-05 00:10:49 +0300

    Add check icon,language and experience to article

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 function Experience() {
   return (
@@ -7,21 +8,22 @@ function Experience() {
     //Add headings for experience
     //Create experience container with two divs frontend and backend
     //Add frontend heading and container for experience details
+    //Add check icon,language and experience
     <section id='experience'>
       <h5>Skills I have</h5>
       <h2>My Experience</h2>
 
       <div className='container experience__container'>
 
-        <div className='experience__frontend'>
-          
+        <div className='experience__frontend'> 
           <h3>Frontend Development</h3>
           
           <div className='experience__content'>
             <article className='experience_details'>
-
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text__light'>Experienced</small>
             </article>
-
           </div>
 
         </div>
