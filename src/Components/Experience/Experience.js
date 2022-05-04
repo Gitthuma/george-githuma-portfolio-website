d462538d2ac54adda76da27488812c0d56e5c0b5
george.githuma <george.githuma@gmail.com>
2022-05-04 23:46:48 +0300

    Create experience container with two divs frontend and backend

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -4,7 +4,26 @@ import './Experience.css'
 function Experience() {
   return (
     //change div to section and add id
-    <section id='experience'>Experience</section>
+    //Add headings for experience
+    //Create experience container with two divs frontend and backend
+    <section id='experience'>
+      <h5>Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+
+        <div className='experience__frontend'>
+
+        </div>
+
+        <div className='experience__backend'>
+
+        </div>
+
+
+      </div>
+    
+    </section>
   )
 }
 
