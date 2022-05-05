eccc2f51300ef5751066f4ed4e1b7845fc604101
george.githuma <george.githuma@gmail.com>
2022-05-06 00:47:18 +0300

    Create container with article inside

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -5,9 +5,17 @@ function Services() {
   return (
     //Change div to section and add id
     //Add headings to services section
+    //Create container with article inside
     <section id='services'>
       <h5>What I offer</h5>
       <h2>Services</h2>
+
+      <div className='container service__container'>
+        <article className='service'>
+
+        </article>
+
+      </div>
       
     
     </section>
