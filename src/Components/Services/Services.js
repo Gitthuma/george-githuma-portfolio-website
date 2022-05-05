0cab8681f4601c174c2e767da9a8abd8c4df7583
george.githuma <george.githuma@gmail.com>
2022-05-06 00:43:21 +0300

    Add headings to services section

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -4,7 +4,13 @@ import './Services.css'
 function Services() {
   return (
     //Change div to section and add id
-    <section id='services'>Services</section>
+    //Add headings to services section
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      
+    
+    </section>
   )
 }
 
