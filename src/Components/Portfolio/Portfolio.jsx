204050138505d854c4a854d86ecd941ee1877256
george.githuma <george.githuma@gmail.com>
2022-05-10 01:31:13 +0300

    Changed file jsx and Create section with headings

---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Portfolio.css'
+
+function Portfolio() {
+  return (
+    //Create section with headings
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+    </section>
+
+  )
+}
+
+export default Portfolio
