1a722bef4296e7c966cf01ee69bf30d36e527c73
george.githuma <george.githuma@gmail.com>
2022-05-09 18:49:27 +0300

    inside article add div of class service head with h3 tag inside and changed js file to jsx

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -6,12 +6,14 @@ function Services() {
     //Change div to section and add id
     //Add headings to services section
     //Create container with article inside
+    //inside article add div of class service head with h3 tag inside
     <section id='services'>
       <h5>What I offer</h5>
       <h2>Services</h2>
 
       <div className='container service__container'>
         <article className='service'>
+          <div
 
         </article>
 
