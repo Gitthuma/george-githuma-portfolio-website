9753dc1a4a826f9bbe5cb7d0f676adafadf86feb
george.githuma <george.githuma@gmail.com>
2022-05-09 18:59:18 +0300

    Duplicate li tag several times

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Services.css'
+import {BiCheck} from 'react-icons/bi'
 
 function Services() {
   return (
@@ -7,14 +8,41 @@ function Services() {
     //Add headings to services section
     //Create container with article inside
     //inside article add div of class service head with h3 tag inside
+    //inside article create list with icon and paragraph after div
+    //Duplicate li tag several times
     <section id='services'>
       <h5>What I offer</h5>
       <h2>Services</h2>
 
       <div className='container service__container'>
+        
         <article className='service'>
-          <div
+          <div className="service__head">
+            <h3>Web Applications</h3>
+          </div>
 
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
         </article>
 
       </div>
