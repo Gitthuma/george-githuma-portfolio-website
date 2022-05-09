6233c001f9eddbc0c67cf2d7b85fc148c2800ac4
george.githuma <george.githuma@gmail.com>
2022-05-09 19:06:44 +0300

    Duplicate article two times to include Mentorships and trainings and Speaking engagements

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -10,15 +10,80 @@ function Services() {
     //inside article add div of class service head with h3 tag inside
     //inside article create list with icon and paragraph after div
     //Duplicate li tag several times
+    //Duplicate article two times to include Mentorships and trainings and Speaking engagements
     <section id='services'>
       <h5>What I offer</h5>
       <h2>Services</h2>
 
       <div className='container service__container'>
+
+        {/*Start of Web Applications*/}
         
         <article className='service'>
           <div className="service__head">
             <h3>Web Applications</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/*Start of Trainings*/}
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>Mentorship & Trainings</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/*Start of Speaking Engagements*/}
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>Speaking Engagements</h3>
           </div>
 
           <ul className='service__list'>
