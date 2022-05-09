00fe508a18e023f070f121d8235947654e7ed6c9
george.githuma <george.githuma@gmail.com>
2022-05-10 01:42:58 +0300

    Inside article, add image, item title and links to github and live demo

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -5,13 +5,18 @@ function Portfolio() {
   return (
     //Create section with headings
     //Create container and portfolio__container class with article of portfolio__item
+    //Inside article, add image, item title and links to github and live demo
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
+        
         <article className='portfolio__item'>
-
+          <div className="portfolio__item-image"></div>
+          <h3>Portfolio Item Title</h3>
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary'>Github</a>
         </article>
 
       </div>
