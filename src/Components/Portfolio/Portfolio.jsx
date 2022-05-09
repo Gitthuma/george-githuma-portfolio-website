520e97c0edde0fb08ea2dfbd5f2824d71490e9cb
george.githuma <george.githuma@gmail.com>
2022-05-10 01:37:00 +0300

    Create container and portfolio__container class with article of portfolio__item

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -4,9 +4,17 @@ import './Portfolio.css'
 function Portfolio() {
   return (
     //Create section with headings
+    //Create container and portfolio__container class with article of portfolio__item
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+
+        </article>
+
+      </div>
 
     </section>
 
