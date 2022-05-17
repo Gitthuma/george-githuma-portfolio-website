2c35268196758f85731a856d95bb081fde58b215
george.githuma <george.githuma@gmail.com>
2022-05-17 22:26:33 +0300

    import image and apply it, and add portfolio image to assets

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../Assets/portfolio-1.png'
 
 function Portfolio() {
   return (
     //Create section with headings
     //Create container and portfolio__container class with article of portfolio__item
     //Inside article, add image, item title and links to github and live demo
+    //import image and apply it
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
@@ -13,7 +15,9 @@ function Portfolio() {
       <div className="container portfolio__container">
         
         <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="First portfolio" />
+          </div>
           <h3>Portfolio Item Title</h3>
           <a href="" className='btn' target='_blank'>Github</a>
           <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
