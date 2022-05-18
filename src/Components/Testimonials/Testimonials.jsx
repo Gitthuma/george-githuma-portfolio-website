29af21ae16ede656e32b9cdd161a213c8ab90c4e
george.githuma <george.githuma@gmail.com>
2022-05-19 01:31:03 +0300

    Added Avatar 1 to 4 in assets, imported it in Testimonials.jsx and applied it

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import './testimonial.css'
+import './Testimonial.css'
+import AVTR1 from '../../Assets/Avatar-1.jpeg'
+import AVTR2 from '../../Assets/Avatar-2.jpeg'
+import AVTR3 from '../../Assets/Avatar-3.jpeg'
+import AVTR4 from '../../Assets/Avatar-4.jpeg'
 
 function Testimonials() {
   //Add headings
   //Create div for testimonial container with article inside
+  //Add Image, client name and review
   return (
     <section id='Testimonials'>
       <h5>Review From Clients</h5>
@@ -11,6 +16,16 @@ function Testimonials() {
 
       <div className="container testimonials__container">
         <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5 className="client__name">Randy Davo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Adipisci soluta odio debitis sequi dignissimos dicta nesciunt 
+              accusantium laboriosam accusamus veniam assumenda omnis, est, 
+              esse ducimus corrupti modi sunt illum in.
+            </small>
+          </div>
 
         </article>
 
