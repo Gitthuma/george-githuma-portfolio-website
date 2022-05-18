afa72c15e31b0c53df814af5a00e648ec96aeb41
george.githuma <george.githuma@gmail.com>
2022-05-18 06:27:42 +0300

    Create array for mapping the different portfolios

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -4,6 +4,58 @@ import IMG1 from '../../Assets/portfolio-1.png'
 import IMG2 from '../../Assets/portfolio-2.png'
 import IMG3 from '../../Assets/portfolio-3.png'
 
+//Create array for mapping the different portfolios
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'George Githuma Portfolio Website',
+    github: 'https://github.com/Gitthuma/george-githuma-portfolio-website',
+    demo: ''
+  },
+
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Amazone Clone',
+    github: 'https://github.com/Gitthuma/amazon-clone',
+    demo: ''
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Title 3',
+    github: 'https://github.com',
+    demo: ''
+  },
+
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Title 4',
+    github: 'https://github.com',
+    demo: ''
+  },
+
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Title 5',
+    github: 'https://github.com',
+    demo: ''
+  },
+
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Title 6',
+    github: 'https://github.com',
+    demo: ''
+  }
+  
+]
+
 function Portfolio() {
   return (
     //Create section with headings
@@ -12,6 +64,7 @@ function Portfolio() {
     //import image and apply it
     //Duplicate article
     //Add a div to links with a classname of portfolio__item-cta
+    
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
