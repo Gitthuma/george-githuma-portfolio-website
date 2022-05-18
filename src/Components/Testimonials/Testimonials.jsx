49d253664d0abb085db267ff1db1cf20f1503887
george.githuma <george.githuma@gmail.com>
2022-05-19 01:06:33 +0300

    Changed js to jsx and Add headings

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Testimonials() {
+  //Add headings
+  return (
+    <section id='Testimonials'>
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+
+    </section>
+  )
+}
+
+export default Testimonials
