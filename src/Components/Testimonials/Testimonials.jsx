02b36c26d12faff3d18f58484840d7c3d83bb77a
george.githuma <george.githuma@gmail.com>
2022-05-19 01:32:28 +0300

    Duplicate article

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -9,12 +9,14 @@ function Testimonials() {
   //Add headings
   //Create div for testimonial container with article inside
   //Add Image, client name and review
+  //Duplicate article
   return (
     <section id='Testimonials'>
       <h5>Review From Clients</h5>
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
+        
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Avatar one" />
@@ -26,7 +28,45 @@ function Testimonials() {
               esse ducimus corrupti modi sunt illum in.
             </small>
           </div>
+        </article>
 
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5 className="client__name">Randy Davo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Adipisci soluta odio debitis sequi dignissimos dicta nesciunt 
+              accusantium laboriosam accusamus veniam assumenda omnis, est, 
+              esse ducimus corrupti modi sunt illum in.
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5 className="client__name">Randy Davo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Adipisci soluta odio debitis sequi dignissimos dicta nesciunt 
+              accusantium laboriosam accusamus veniam assumenda omnis, est, 
+              esse ducimus corrupti modi sunt illum in.
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5 className="client__name">Randy Davo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Adipisci soluta odio debitis sequi dignissimos dicta nesciunt 
+              accusantium laboriosam accusamus veniam assumenda omnis, est, 
+              esse ducimus corrupti modi sunt illum in.
+            </small>
+          </div>
         </article>
 
       </div>
