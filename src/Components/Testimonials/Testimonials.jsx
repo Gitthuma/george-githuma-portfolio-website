7e3d7531c188b7d505d5565b2ce8e69025795294
george.githuma <george.githuma@gmail.com>
2022-05-19 02:59:12 +0300

    import Swiper core and required modules and swiper styles

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -5,6 +5,17 @@ import AVTR2 from '../../Assets/Avatar-2.jpeg'
 import AVTR3 from '../../Assets/Avatar-3.jpeg'
 import AVTR4 from '../../Assets/Avatar-4.jpeg'
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 //Add array of testimonial onbjects to generate testimony dynamicaly
 const data = [
   {
