485c165559c4476746e3d06607f38854fc86a3d2
george.githuma <george.githuma@gmail.com>
2022-05-19 01:13:05 +0300

    Create div for testimonial container with article inside

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 function Testimonials() {
   //Add headings
+  //Create div for testimonial container with article inside
   return (
     <section id='Testimonials'>
       <h5>Review From Clients</h5>
       <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+
+        </article>
+
+      </div>
 
     </section>
   )
