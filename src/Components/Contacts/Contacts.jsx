ac295e429415d7f57f3bf13d95403ea6295422c3
george.githuma <george.githuma@gmail.com>
2022-05-24 14:40:13 +0300

    Add contacts__container class with contact options inside

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -5,9 +5,18 @@ function Contacts() {
   return (
     //change div to section and add id
     //Add headings
+    //Add contacts__container class with contact options inside
     <section id='contacts'>
       <h5>Get In Touch</h5>
       <h2>Contacts</h2>
+
+      <div className="container contacts__container">
+        
+        <div className="contacts__options">
+
+        </div>
+
+      </div>
 
     </section>
   )
