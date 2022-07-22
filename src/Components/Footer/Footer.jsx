06f1e61e40387427bd522a2752f67d8b5c0f7955
george.githuma <george.githuma@gmail.com>
2022-07-22 22:08:24 +0300

    Corrected Home Link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
       <a href="#" className='footer_logo'>georgegithuma.com</a>
 
       <ul className='permalinks'>
-        <li><a href="#"></a>Home</li>
+        <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
