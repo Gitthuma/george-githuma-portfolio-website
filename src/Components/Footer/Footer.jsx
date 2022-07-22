ef1b36ece3c383966dfbe9c79abedb01367a789d
george.githuma <george.githuma@gmail.com>
2022-07-22 08:56:37 +0300

    link to logo and creation of permalinks

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,31 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    
+    //link to logo
+    //create permalinks
+    //Add socials
+
+    <footer>
+      
+      <a href="#" className='footer_logo'>george githima</a>
+
+      <ul className='permalinks'>
+        <li><a href="#"></a>Home</li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        
+      </div>
+
+
+    </footer>
   )
 }
 
